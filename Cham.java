cd0a91b4e7fd28f9d3b67b8ed7bdbfdefb74736e
cham2601 <chamanthyarlagadda@gmail.com>
2019-06-27 03:35:18 +0530

    first commit

---
+++ Cham.java
@@ -0,0 +1,18 @@
+
+public class Cham {
+
+	public Cham() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("7");
+		System.out.println("7<-46");
+		System.out.println("7");
+		System.out.println("7<-46<-29");
+		System.out.println("7<-46<-29<-42");
+		System.out.println("7<-46<-29<-42<-41");
+		System.out.println("7<-46<-29<-42<-41<-0");
+	}
+}
